a3112d3690241a2f912b608bc02a4737ea21b9fa
jcm0314 <143155281+jcm0314@users.noreply.github.com>
2025-04-30 16:49:03 +0900

    Render 배포를 위한 PR (#3)
    * serviceAccountKey.json .gitignore에서 제거
    
    * serviceAccountKey.json .gitignore에서 제거
    
    * firebase admin SDK
    
    admin SDK 강제 추가
    
    * 테스트
    
    * Delete serviceAccountKey.json
    
    * 환경변수에서 직접 읽어서 초기
    
    * Render
    
    * .gitignore에 ServiceAccount.json 추가
    
    ---------
    
    Co-authored-by: kimshyuno3 <kimgrace030103@naver.com>

--- controllers/passwordResetController.js
+++ controllers/passwordResetController.js
@@ -7,7 +7,7 @@ const { isPasswordValid, passwordRequirementMessage } = require('../validators/p
 
 
 // Firebase 초기화
-const serviceAccount = require('../serviceAccountKey.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
